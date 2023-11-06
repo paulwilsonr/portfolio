8ff2724251d84c7de615cb140bf2d8d8a76910f2
Paul Wilson <paul.wilson.r@gmail.com>
2023-11-06 11:57:55 -0600

    Fix mobile navBar bug

--- src/util/useElementOnScreen.ts
+++ src/util/useElementOnScreen.ts
@@ -1,17 +1,25 @@
 import { useState, useEffect, RefObject } from 'react'
 
+
+
 const options = {
   root: null,
-  rootMargin: '20%',
+  rootMargin: '100%',
   threshold: 1
 }
 
 const useElementOnScreen = (locationRef: RefObject<HTMLDivElement>) => {
   const [isIntersecting, setIsIntersecting] = useState(false)
-
+  if(window.innerWidth < 480) {
+    options.rootMargin = '100%'
+  } else {
+    options.rootMargin = '20%'
+  }
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
       setIsIntersecting(entry.isIntersecting)
+     
+      console.log(options)
     }, options)
     if (locationRef !== null) {
       observer.observe(locationRef.current)
